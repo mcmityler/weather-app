dd9a820dfed332435a111d6c266f9cfb99797802
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-07-02 11:39:52 -0400

    Add async func to get weather from weather api

--- src/index.js
+++ src/index.js
@@ -15,3 +15,27 @@ document.body.appendChild(image);
 let hi = "hi";
 let hello = () => console.log("hey");
 hello();
+
+import weatherBrain from "./weather.js";
+
+console.log(
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Port%20Elgin?unitGroup=us&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json",
+  ),
+);
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=XZ7QS88NDFT6EPKPV35ZNL5ZW
+
+async function getWeather(place, tempUnit) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${tempUnit}&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json`,
+    );
+    const weatherData = await response.json();
+    console.log("fetched data");
+    console.log(weatherData.days[0].temp);
+    console.log(weatherData);
+  } catch (error) {
+    console.error("Error fetching the Weather:", error);
+  }
+}
+getWeather("Toronto", "us");
