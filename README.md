8ae6182cbd9982ecddf2e16d7515de97cc0ff7d0
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-07-07 17:51:53 -0400

    Add functionality to find weather in a city

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# template-webpack
-Template for building websites using webpack
+# Weather App
+
+Application using visual crossing API to get weather and display to user
+
+Bebas Neue Font: https://fonts.google.com/specimen/Bebas+Neue?preview.script=Latn
+color swatch: https://colorhunt.co/palette/a9eee6fefaecf38181625772
--- src/index.js
+++ src/index.js
@@ -1,41 +1,157 @@
-// src/index.js
+// // src/index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
+const errorText = document.querySelector(".error");
+const cityText = document.querySelector(".city-name");
+const weatherText = document.querySelector(".weather");
+const temperatureText = document.querySelector(".temperature");
+const unitText = document.querySelector(".unit-text");
+const lastUpdated = document.querySelector(".last-updated");
+const updateButton = document.querySelector(".update-button");
 
-// src/index.js
-import testImage from "./ramenTest.jpg";
+const form = document.querySelector("form");
+const celsiusRadio = document.getElementById("celsius");
+const timeTakenText = document.getElementById("api-time");
 
-const image = document.createElement("img");
-image.src = testImage;
+let place = "";
+let tempUnit = "";
+let errorPlace = "";
 
-document.body.appendChild(image);
-
-let hi = "hi";
-let hello = () => console.log("hey");
-hello();
-
-import weatherBrain from "./weather.js";
-
-console.log(
-  fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Port%20Elgin?unitGroup=us&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json",
-  ),
-);
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=XZ7QS88NDFT6EPKPV35ZNL5ZW
-
-async function getWeather(place, tempUnit) {
+async function getWeather(m_place, m_tempUnit) {
   try {
+    errorPlace = m_place;
+    // 1. Capture start time
+    const startTime = performance.now();
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=${tempUnit}&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${m_place}?unitGroup=${m_tempUnit}&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json`,
     );
-    const weatherData = await response.json();
-    console.log("fetched data");
-    console.log(weatherData.days[0].temp);
-    console.log(weatherData);
+    // 3. Capture end time
+    const endTime = performance.now();
+
+    // 4. Calculate total time in milliseconds
+    const timeTaken = endTime - startTime;
+
+    timeTakenText.textContent = `Task took ${timeTaken.toFixed(2)} ms to finish.`;
+    console.log(response);
+    if (response.ok === true) {
+      place = m_place;
+      tempUnit = m_tempUnit;
+      const weatherData = await response.json();
+      console.log("fetched data");
+      console.log(weatherData);
+      return weatherData;
+    }
   } catch (error) {
     console.error("Error fetching the Weather:", error);
   }
 }
-getWeather("Toronto", "us");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+  const formData = new FormData(event.target);
+  const city = formData.get("city-input");
+  const unit = formData.get("temp-unit") === "C" ? "uk" : "us";
+  console.log(formData.entries());
+  console.log(city);
+  console.log(unit);
+  getWeather(city, unit).then((response) => {
+    UpdateWeather(response);
+    if (response != undefined) {
+      UpdateTemperatureUnit(unit);
+    }
+  });
+
+  //Not needed
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
+
+updateButton.addEventListener("click", () => {
+  getWeather(place, celsiusRadio.checked ? "uk" : "us").then((response) => {
+    UpdateWeather(response);
+    if (response != undefined) {
+      UpdateTemperatureUnit(celsiusRadio.checked ? "uk" : "us");
+    }
+  });
+});
+
+function UpdateWeather(response) {
+  if (response === undefined) {
+    //return if the address isnt being found
+    errorText.textContent = "Can't find location: " + errorPlace;
+    return;
+  }
+  errorText.textContent = "";
+
+  console.log(response);
+  cityText.textContent = response.resolvedAddress;
+  console.log(response.days[0].temp);
+  temperatureText.textContent = response.days[0].temp;
+
+  weatherText.textContent = response.days[0].conditions;
+
+  lastUpdated.textContent =
+    "last updated: " +
+    new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit", // Optional
+      hour12: true,
+    });
+}
+
+function UpdateTemperatureUnit(unit) {
+  console.log(unit);
+  switch (unit) {
+    case "uk":
+      unitText.textContent = "°C";
+      break;
+    case "us":
+      unitText.textContent = "°F";
+      break;
+  }
+}
+
+getWeather("Port Elgin", "uk")
+  .then((response) => {
+    UpdateWeather(response);
+  })
+  .catch((error) => {
+    console.error("Error fetching the Gif:", error);
+  });
+
+/* <label for="city-input">Enter a city:</label>
+        <input type="text" id="city-input" />
+        <button>Search</button>
+        <input type="radio" name="temp-unit" id="Celsius" value="C" checked />
+        <label for="Celsius">Celsius</label>
+        <input type="radio" name="temp-unit" id="fahrenheit" value="F" />
+        <label for="fahrenheit">Fahrenheit</label>
+        */
+
+// import { greeting } from "./greeting.js";
+
+// console.log(greeting);
+
+// // src/index.js
+// import testImage from "./ramenTest.jpg";
+
+// const image = document.createElement("img");
+// image.src = testImage;
+
+// document.body.appendChild(image);
+
+// let hi = "hi";
+// let hello = () => console.log("hey");
+// hello();
+
+// import weatherBrain from "./weather.js";
+
+// console.log(
+//   fetch(
+//     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Port%20Elgin?unitGroup=us&key=XZ7QS88NDFT6EPKPV35ZNL5ZW&contentType=json",
+//   ),
+// );
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=XZ7QS88NDFT6EPKPV35ZNL5ZW
